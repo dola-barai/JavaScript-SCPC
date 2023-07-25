0b44ea3e299f3ff8078fa5f933ba51fae388af6b
Dola Barai <dola43dc@gmail.com>
2023-07-25 22:44:07 +0600

    reverse and positive sum

---
+++ first.js
@@ -0,0 +1,11 @@
+function reverseString(inputString) {
+    let reversedString = '';
+    for (let i = inputString.length - 1; i >= 0; i--) {
+      reversedString += inputString[i];
+    }
+    return reversedString;
+}
+
+console.log(reverseString("hello world")); 
+
+  
